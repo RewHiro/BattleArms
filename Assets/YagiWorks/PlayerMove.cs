eb369f9c6deda28a5a130dfb2ced3b66df971d54
KOKI YAGI <kappa_sense_pen@yahoo.co.jp>
2015-11-26 16:49:59 +0900

    学校の作業分

--- Assets/YagiWorks/PlayerMove.cs
+++ Assets/YagiWorks/PlayerMove.cs
@@ -7,27 +7,42 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
     }
+    public float force = 100.0f;
+    public int jumpCount = 0;       // ジャンプした回数
+    public const int MAX_JUMP_COUNT = 2;    // ジャンプできる最大回数
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            jumpCount = 0; // ジャンプ回数初期化
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up")|| Input.GetKey("w"))
         {
             transform.position += transform.forward ;
         }
-        if (Input.GetKey("down"))
+        if (Input.GetKey("down")|| Input.GetKey("s"))
         {
             transform.position -= transform.forward;
         }
 
-        if (Input.GetKey("right"))
+        if (Input.GetKey("right")|| Input.GetKey("e"))
         {
             transform.Rotate(0, 5, 0);
         }
-        if (Input.GetKey("left"))
+        if (Input.GetKey("left")|| Input.GetKey("q"))
         {
             transform.Rotate(0, -5, 0);
         }
-       
+        if (jumpCount < MAX_JUMP_COUNT && Input.GetKey("x"))
+        {
+            jumpCount++;
+            GetComponent<Rigidbody>().AddForce(transform.up * force, ForceMode.Impulse);
+        }
     }
 }
