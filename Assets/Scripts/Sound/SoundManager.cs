b96e3ecf25a99edbeea4e5beeb41688914cb103d
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-12-03 09:24:33 +0900

    画像、プレハブをResourceに移動、サウンド管理実装

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SoundManager : MonoBehaviour
+{
+
+    List<AudioSource> bgm_list_ = new List<AudioSource>();
+    List<AudioSource> se_list_ = new List<AudioSource>();
+
+    [SerializeField]
+    AudioClip[] bgms_ = null;
+
+    [SerializeField]
+    AudioClip[] ses_ = null;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        for (int i = 0; i < bgms_.Length; ++i)
+        {
+            var audio_source = new AudioSource();
+            audio_source.clip = bgms_[i];
+            bgm_list_.Add(audio_source);
+        }
+
+        for (int i = 0; i < bgms_.Length; ++i)
+        {
+            var audio_source = new AudioSource();
+            audio_source.clip = ses_[i];
+            se_list_.Add(audio_source);
+        }
+    }
+
+    void PlaySE(int num)
+    {
+        if (se_list_[num].isPlaying) return;
+        se_list_[num].Play();
+    }
+
+    void PlayBGM(int num)
+    {
+        if (bgm_list_[num].isPlaying) return;
+        bgm_list_[num].Play();
+    }
+
+    void StopSE()
+    {
+        var itr = se_list_.GetEnumerator();
+        while (itr.MoveNext())
+        {
+            itr.Current.Stop();
+        }
+    }
+
+    void StopBGM()
+    {
+        var itr = bgm_list_.GetEnumerator();
+        while (itr.MoveNext())
+        {
+            itr.Current.Stop();
+        }
+    }
+}
