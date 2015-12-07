a6bed647d8d0a57bbbeacdb2e825be4ce96e9604
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-12-07 16:22:50 +0900

    敵AI更新

--- Assets/Scripts/Scenes/StageSelect/StageSelectClicker.cs
+++ Assets/Scripts/Scenes/StageSelect/StageSelectClicker.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
 public class StageSelectClicker : MonoBehaviour
 {
+    List<string> stage_name_list_ = new List<string>();
+
+    void Start()
+    {
+        stage_name_list_.Add("stage01");
+        stage_name_list_.Add("stage02");
+        stage_name_list_.Add("stage03");
+    }
+
     public void Ready(int num)
     {
+        FindObjectOfType<MyNetworkLobbyManager>().playScene = stage_name_list_[num];
         FindObjectOfType<SoundManager>().PlaySE(4);
         FindObjectOfType<StageData>().stageNum = num;
         var network_lobby_player = FindObjectOfType<NetworkLobbyPlayer>();
