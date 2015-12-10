6648cb5dc905bebad75fb6012d18bbb50ed0ff1e
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-12-10 11:30:27 +0900

    hitエフェクト実装

---
+++ Assets/Scripts/Bullet/HitEffector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitEffector : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject effect_ = null;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        var effect = Instantiate(effect_);
+        effect_.transform.position = gameObject.transform.position;
+        Destroy(effect, 4.0f);
+    }
+}
