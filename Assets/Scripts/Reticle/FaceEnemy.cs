cd987b01b921b5bd4b7645d736ab050326c22e03
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-12-20 18:27:31 +0900

    ロックオンの切り替え、機体が傾かないように修正、Baseタグ追加

--- Assets/Scripts/Reticle/FaceEnemy.cs
+++ Assets/Scripts/Reticle/FaceEnemy.cs
@@ -6,21 +6,19 @@ using UnityEngine.Networking;
 public class FaceEnemy : NetworkBehaviour
 {
 
-    GameObject lock_enemy_;
-    GameObject start_lock_enemy_;
+    GameObject[] enemy_;
+    GameObject[] base_;
+    GameObject look_object_;
+    Vector3 enemy_position;
 
-    int enemy_number;
-
-    TargetCircle target_circle_;
-
+    int enemy_number_;
+    int base_number_;
 
     public override void OnStartLocalPlayer()
     {
-        enemy_number = 0;
-        target_circle_ = GetComponentInChildren<TargetCircle>();
 
-        //start_lock_enemy_ = target_circle_.GetEnemyObject;
-        //lock_enemy_ = start_lock_enemy_;
+        enemy_ = GameObject.FindGameObjectsWithTag("Enemy");
+        base_ = GameObject.FindGameObjectsWithTag("Base");
 
         base.OnStartLocalPlayer();
     }
@@ -28,14 +26,61 @@ public class FaceEnemy : NetworkBehaviour
     void Update()
     {
         if (!isLocalPlayer) return;
-        target_circle_ = GetComponentInChildren<TargetCircle>();
-
-        lock_enemy_ = target_circle_.GetEnemyObject;
-
-        if (lock_enemy_ == null) return;
-
-        //transform.Rotate(0, 12, 0);
-
-        transform.LookAt(lock_enemy_.transform);
+        IsChangeEnemyNumber();
+        LookAtEnemy();
     }
+
+    public void ChangeLockEnemy()
+    {
+        enemy_number_++;
+
+        if (enemy_number_ >= enemy_.Length)
+        {
+            base_number_++;
+        }
+
+        if (base_number_ >= base_.Length)
+        {
+            base_number_ = -1;
+            enemy_number_ = 0;
+
+        }
+
+    }
+
+
+    public void IsChangeEnemyNumber()
+    {
+        if (!(enemy_.Length == GameObject.FindGameObjectsWithTag("Enemy").Length))
+        {
+            enemy_ = GameObject.FindGameObjectsWithTag("Enemy");
+        }
+
+        if (!(base_.Length == GameObject.FindGameObjectsWithTag("Base").Length))
+        {
+            base_ = GameObject.FindGameObjectsWithTag("Base");
+        }
+
+    }
+
+    public void LookAtEnemy()
+    {
+        if (enemy_number_ < enemy_.Length)
+        {
+            look_object_ = enemy_[enemy_number_];
+        }
+        else
+        if (enemy_number_ >= enemy_.Length)
+        {
+            look_object_ = base_[base_number_];
+        }
+
+        enemy_position = look_object_.transform.position;
+        enemy_position.y = gameObject.transform.position.y;
+
+        gameObject.transform.LookAt(enemy_position);
+
+    }
+
+
 }
