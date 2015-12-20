72870a157845aad7e01d6d7ea67ab55ed159f8c4
kazukin134 <kazukin1345@gmail.com>
2015-12-20 17:08:35 +0900

    敵のターゲットの切り替えとx軸に回転しないようにする

---
+++ Assets/Scripts/Reticle/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    FaceEnemy face_enemy_;
+
+	// Use this for initialization
+	void Start () {
+        face_enemy_ = GetComponent<FaceEnemy>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            face_enemy_.ChangeLockEnemy();                                                                                                                                                                                                                                                                                                                                                  //homo
+        }
+	}
+}
